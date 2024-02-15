01a27e90118a055d73e8b408e3b966b024818750
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-02-15 14:55:55 +0100

    Add debug option for requests

--- src/services/config-notification.ts
+++ src/services/config-notification.ts
@@ -6,8 +6,10 @@ const PREFIX_CONFIG_NOTIFICATION_WS = `${process.env.REACT_APP_WS_GATEWAY}/confi
 
 export function connectNotificationsWsUpdateConfig(): ReconnectingWebSocket {
     const webSocketUrl = `${getWsBase()}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?appName=${APP_NAME.toLowerCase()}`;
-    const reconnectingWebSocket = new ReconnectingWebSocket(() =>
-        getUrlWithToken(webSocketUrl)
+    const reconnectingWebSocket = new ReconnectingWebSocket(
+        () => getUrlWithToken(webSocketUrl),
+        undefined,
+        { debug: process.env.REACT_APP_DEBUG_REQUESTS === 'true' }
     );
     reconnectingWebSocket.onopen = function (event: Event) {
         console.info(
