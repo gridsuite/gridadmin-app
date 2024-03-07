556494cf426de2a05e050a6ef4a642a0901d1b14
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-03-07 19:30:50 +0100

    fix copyright headers

--- src/services/config-notification.ts
+++ src/services/config-notification.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import ReconnectingWebSocket, { Event } from 'reconnecting-websocket';
 import { APP_NAME } from '../utils/config-params';
 import { getUrlWithToken, getWsBase } from '../utils/api-ws';
--- src/services/study.ts
+++ src/services/study.ts
@@ -1,9 +1,10 @@
-/**
+/*
  * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 import { backendFetchJson, Token } from '../utils/api-rest';
 
 const STUDY_URL = `${process.env.REACT_APP_API_GATEWAY}/study/v1`;
