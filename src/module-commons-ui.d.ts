e7069abc5191ff9a7e00894ebfa1af464d13c407
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-03-07 22:55:16 +0100

    remove comments

--- src/module-commons-ui.d.ts
+++ src/module-commons-ui.d.ts
@@ -1,4 +1,2 @@
 //TODO: remove when commons-ui will include typescript definitions
-declare module '@gridsuite/commons-ui' /*{
-    export = typeof import('@gridsuite/commons-ui');
-}*/;
+declare module '@gridsuite/commons-ui';
