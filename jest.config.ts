35d468c36a817468ad9e58578647bfc1e1f90cfb
Florent MILLOT <millotflo@gmail.com>
2024-04-16 13:03:42 +0200

    Remove file, add comment
    Signed-off-by: Florent MILLOT <millotflo@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         '^.+\\.svg\\?react$': 'jest-svg-transformer',
         '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
     },
+    // if need to test with AG Grid, see https://www.ag-grid.com/react-data-grid/testing/
     transformIgnorePatterns: ['node_modules/(?!@gridsuite/commons-ui)'], // transform from ESM
     moduleDirectories: ['node_modules', 'src'], // to allow absolute path from ./src
     setupFiles: ['<rootDir>/jest.setup.ts'],
