d2c6b35c11d9f77e61ccf6804e0eadff7e5314b5
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-05-06 18:39:18 +0200

    remove tokenId from notificationsUrlKeys

--- src/utils/notifications-provider.ts
+++ src/utils/notifications-provider.ts
@@ -6,31 +6,22 @@
  */
 
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import type { AppState } from '../redux/reducer';
-import { getUrlWithToken, getWsBase } from './api-ws';
+import { getWsBase } from './api-ws';
 import { APP_NAME } from './config-params';
 import { NotificationsUrlKeys, PREFIX_CONFIG_NOTIFICATION_WS } from '@gridsuite/commons-ui';
 
 export function useNotificationsUrlGenerator() {
     // The websocket API doesn't allow relative urls
     const wsBase = getWsBase();
-    const tokenId = useSelector((state: AppState) => state.user?.id_token);
 
-    // return a mapper with NOTIFICATIONS_URL_KEYS and undefined value if URL is not yet buildable (tokenId)
+    // return a mapper with NOTIFICATIONS_URL_KEYS value
     // it will be used to register listeners as soon as possible.
     return useMemo(
         () =>
             ({
-                [NotificationsUrlKeys.CONFIG]: tokenId
-                    ? getUrlWithToken(
-                          `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${new URLSearchParams({ appName: APP_NAME })}`
-                      )
-                    : undefined,
-                [NotificationsUrlKeys.GLOBAL_CONFIG]: tokenId
-                    ? getUrlWithToken(`${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/global`)
-                    : undefined,
-            }) satisfies Partial<Record<NotificationsUrlKeys, string | undefined>>,
-        [wsBase, tokenId]
+                [NotificationsUrlKeys.CONFIG]:`${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/notify?${new URLSearchParams({ appName: APP_NAME })}`,
+                [NotificationsUrlKeys.GLOBAL_CONFIG]: `${wsBase}${PREFIX_CONFIG_NOTIFICATION_WS}/global`,
+            }) satisfies Partial<Record<NotificationsUrlKeys, string>>,
+        [wsBase]
     );
 }
