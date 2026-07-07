f30e462349487c19976f38f1f815694eb1eaf27d
Ghiles ABDELLAH <ghiles.abdellah@rte-france.com>
2026-07-07 15:46:14 +0200

    feat: add operation quota reset button

--- src/services/user-admin.ts
+++ src/services/user-admin.ts
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { backendFetch, backendFetchJson } from '@gridsuite/commons-ui';
+import { backendFetch, backendFetchJson, safeEncodeURIComponent } from '@gridsuite/commons-ui';
 import { getRestBase } from '../utils/api-rest';
 import type { UUID } from 'node:crypto';
 
@@ -65,6 +65,18 @@ export function updateUser(userInfos: UserInfosUpdate) {
         .then(() => undefined)
         .catch((reason) => {
             console.error(`Error while updating user : ${reason}`);
+            throw reason;
+        });
+}
+
+export function resetUserCurrentQuotaUsage(sub: string): Promise<void> {
+    console.debug(`Resetting current quota usage for user "${sub}"...`);
+    return backendFetch(`${USER_ADMIN_URL}/users/${safeEncodeURIComponent(sub)}/quota/reset`, {
+        method: 'post',
+    })
+        .then(() => undefined)
+        .catch((reason) => {
+            console.error(`Error while resetting user quota usage : ${reason}`);
             throw reason;
         });
 }
